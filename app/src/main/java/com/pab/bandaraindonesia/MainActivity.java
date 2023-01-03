97d68ffb52d7129ab41d3bae95a34ff5857c501f
Marceloslife <114385677+MuhamadDafhiMayrizkiy@users.noreply.github.com>
2023-01-04 02:00:28 +0700

    Implicit
    Perpindahan mainactivity ke second activity

--- app/src/main/java/com/pab/bandaraindonesia/MainActivity.java
+++ app/src/main/java/com/pab/bandaraindonesia/MainActivity.java
@@ -2,13 +2,25 @@ package com.pab.bandaraindonesia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_mulai;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_mulai = (Button) findViewById(R.id.btn_mulai);
+        btn_mulai.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent PINDAH = new Intent(MainActivity.this, Second.class);
+                startActivity(PINDAH);
+            }
+        });
+
     }
 }
